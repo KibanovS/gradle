f3d01f8911dbe07cb752f62c88bd1bddd02e186b
Slava Kibanov <kibanchous@mail.ru>
2024-02-13 20:47:28 +0500

    changes

--- src/test/java/CashBackHackServiceTest.java
+++ src/test/java/CashBackHackServiceTest.java
@@ -1,23 +1,25 @@
-public class CashBackServiceTest {
-    @T
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashBackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @org.testng.annotations.Test
     public void shouldCorrectAmount() {
         assertEquals(service.remain(300), 700);
 
     }
 
     @org.testng.annotations.Test
-
     public void shouldAmountZero() {
         assertEquals(service.remain(0), 1000);
     }
 
-    @Test
+    @org.testng.annotations.Test
     public void shouldAmountMoreThan1000() {
-
         assertEquals(service.remain(1400), 600);
     }
 
-    @Test
+    @org.testng.annotations.Test
     public void shouldBuyExtraIfTheAmount1000() {
         assertEquals(service.remain(1000), 0);
     }
